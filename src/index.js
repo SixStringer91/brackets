6354c120af15f5369f42b8228e0d7988163105f1
SixStringer91 <danlootsev@gmail.com>
2021-03-15 17:14:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (!str) return true
+    const newStr = bracketsConfig.reduce((a,b) => a = a.replace(b[0] + b[1], ''),str)
+    return newStr===str ? false : check(newStr, bracketsConfig);
 }
